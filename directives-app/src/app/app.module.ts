9c9060e8327f691dad49fdf8b3fc2ba821841256
Rafael Miranda <rafael.miranda@db1.com.br>
2019-10-09 10:15:28 -0300

    [DEV] Directives - ngStyle

---
+++ directives-app/src/app/app.module.ts
@@ -0,0 +1,26 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { NgStyleComponent } from './ng-style/ng-style.component';
+import {FormsModule} from '@angular/forms';
+import { MatSelectModule, MatRadioModule, MatCardModule} from '@angular/material';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NgStyleComponent
+  ],
+  imports: [
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule, 
+    MatSelectModule,
+    MatRadioModule,
+    MatCardModule,
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
